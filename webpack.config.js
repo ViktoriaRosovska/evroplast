d1e5f997fe45624c7939d10ca360ccb91b079c23
Viktoria Rosovska <torimeister@gmail.com>
2024-02-07 15:30:14 +0200

    project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
+const FileIncludeWebpackPlugin = require("file-include-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -10,21 +12,41 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        loader: "ts-loader",
         exclude: /node_modules/,
+        include: path.resolve(__dirname, "src"),
       },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
     ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
+    publicPath: "/",
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+  },
   plugins: [
+    new FileIncludeWebpackPlugin({
+      source: "./src/templates", // relative path to your templates
+      filename: "index.html",
+      inject: true,
+      replace: [
+        {
+          regex: /\[\[FILE_VERSION]]/, // additional things to replace
+          to: "v=1.0.0",
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: path.resolve(__dirname, "dist", "index.html"),
+      inject: false,
     }),
   ],
 };
